ce9b8c8e8b31973ab0f35c00ec131c7e90a969fe
artem <artem@andraft.com>
2013-03-19 09:40:27 +0400

    android3dgo

--- SurfsUp/src/ru/andraft/surfsup/Rawik/RajawaliRendererka.java
+++ SurfsUp/src/ru/andraft/surfsup/Rawik/RajawaliRendererka.java
@@ -2,45 +2,48 @@ package ru.andraft.surfsup.Rawik;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+
 import rajawali.BaseObject3D;
 import rajawali.lights.DirectionalLight;
-import rajawali.materials.DiffuseMaterial;
+import rajawali.materials.GouraudMaterial;
+
 import rajawali.parser.ObjParser;
-import rajawali.primitives.Sphere;
 import rajawali.renderer.RajawaliRenderer;
 import ru.andraft.surfsup.R;
+import android.graphics.Color;
+
 
 public class RajawaliRendererka extends RajawaliRenderer {
     private DirectionalLight mLight;
     private BaseObject3D mObject;
-
     public RajawaliRendererka(Context context) {
         super(context);
         setFrameRate(60);
     }
+    protected void setColorka(BaseObject3D b3d){
+        GouraudMaterial simple = new GouraudMaterial();
+        simple.setUseColor(true);
+        simple.setSpecularIntensity(.1f,.1f,1.f,1);
+        b3d.setMaterial(simple);
+        b3d.setColor(Color.GREEN);
+
+    }
 
     protected void initScene() {
-        mLight = new DirectionalLight(); // set the direction
-       // mLight.setColor(1.0f, 1.0f, 1.0f);
-       mLight.setPower(2);
-        ObjParser objParser = new ObjParser(mContext.getResources(),mTextureManager,R.raw.andrusha);
-        objParser.parse();
-        mObject = objParser.getParsedObject();
-        mObject.setLight(mLight);
-        addChild(mObject);
-
-        mCamera.setZ(-5.2f);
+       mLight = new DirectionalLight(-.5f,-.5f,-.5f);
+       mLight.setPower(5.0f);
+       ObjParser objParser = new ObjParser(mContext.getResources(),mTextureManager,R.raw.andrusha_obj);
+       objParser.parse();
+       mObject = objParser.getParsedObject();
+       mObject.setLight(mLight);
+     setColorka(mObject);
+       mObject.setPosition(.0f,-2.0f,.0f);
+       addChild(mObject);
     }
-
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         super.onSurfaceCreated(gl, config);
-
     }
-
     public void onDrawFrame(GL10 glUnused) {
         super.onDrawFrame(glUnused);
-        mObject.setRotY(mObject.getRotY() + 1);
     }
 }
