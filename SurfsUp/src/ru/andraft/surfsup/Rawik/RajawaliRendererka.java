c7fe93b4723777812902d406fc6df8f1d328cb66
artem <artem@andraft.com>
2013-03-19 09:40:27 +0400

    android3dgo

--- SurfsUp/src/ru/andraft/surfsup/Rawik/RajawaliRendererka.java
+++ SurfsUp/src/ru/andraft/surfsup/Rawik/RajawaliRendererka.java
@@ -2,15 +2,19 @@ package ru.andraft.surfsup.Rawik;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 import rajawali.BaseObject3D;
 import rajawali.lights.DirectionalLight;
-import rajawali.materials.DiffuseMaterial;
 import rajawali.parser.ObjParser;
-import rajawali.primitives.Sphere;
 import rajawali.renderer.RajawaliRenderer;
 import ru.andraft.surfsup.R;
+import android.app.Activity;
+
 
 public class RajawaliRendererka extends RajawaliRenderer {
     private DirectionalLight mLight;
@@ -21,26 +25,23 @@ public class RajawaliRendererka extends RajawaliRenderer {
         setFrameRate(60);
     }
 
+
     protected void initScene() {
-        mLight = new DirectionalLight(); // set the direction
-       // mLight.setColor(1.0f, 1.0f, 1.0f);
+       mLight = new DirectionalLight(); // set the direction
        mLight.setPower(2);
-        ObjParser objParser = new ObjParser(mContext.getResources(),mTextureManager,R.raw.andrusha);
+        ObjParser objParser = new ObjParser(mContext.getResources(),mTextureManager,R.raw.andrushka);
         objParser.parse();
         mObject = objParser.getParsedObject();
         mObject.setLight(mLight);
+        mObject.setPosition(0.8f,0.0f,0.0f);
         addChild(mObject);
-
-        mCamera.setZ(-5.2f);
     }
-
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         super.onSurfaceCreated(gl, config);
-
     }
-
     public void onDrawFrame(GL10 glUnused) {
         super.onDrawFrame(glUnused);
-        mObject.setRotY(mObject.getRotY() + 1);
     }
+
+
 }
